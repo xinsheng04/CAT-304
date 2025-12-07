523518957fbc652b075d11ab14d2b4d06399271f
zh2eng <zh2eng2022@gmail.com>
2025-12-07 13:30:20 +0800

    add percentage for the pillar card

--- frontend/src/component/roadmaps/pillarCard.tsx
+++ frontend/src/component/roadmaps/pillarCard.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { Link, useParams } from 'react-router';
-import { toggleView } from '@/store/pillarsSlice';
+import { toggleView, autosetViewTrue } from '@/store/pillarsSlice';
+import { useSelector } from "react-redux";
+import type { LinkCardProps } from './linkCard';
 
 // Type and data structure
 export interface PillarCardProps {
@@ -25,12 +27,32 @@ const PillarCard : React.FC<PillarCardProps> = ({
     // use link to get roadmap slug
     const { roadmapSlug } = useParams<{ roadmapSlug: string }>();
     const dispatch = useDispatch();
+    const linksData = useSelector((state: any) => state.link.linkList) as LinkCardProps[];
     // toggleViewed indicator
     const handleToggleViewed = (e: React.MouseEvent) => {
-            e.stopPropagation();
-            e.preventDefault();
-            dispatch(toggleView(chapterID))
-        };
+        e.stopPropagation();
+        e.preventDefault();
+        dispatch(toggleView(chapterID))
+    };
+    // percentage generator
+    const generateViewPercentage = (chapterID: number) => {
+        const filtered = linksData.filter(p => p.chapterID === chapterID);
+        const viewArr = filtered.map(p => p.isViewed);
+
+        const viewScore = (level: boolean) => (level ? 1 : 0);
+
+        const getPercentage = (arr: boolean[]) => {
+            const avg = arr.reduce((sum, d) => sum + viewScore(d), 0) / arr.length;
+            return avg;
+        }
+        const percentage = getPercentage(viewArr) * 100;
+        const percentText = Math.round(percentage);
+        if(percentText == 100){
+            dispatch(autosetViewTrue(chapterID))
+        }
+        return percentText
+    }
+
     return (
         <Link to={`/roadmap/${roadmapID}/${roadmapSlug}/${chapterID}/${chapterSlug}`}>
         <div className={`
@@ -50,6 +72,20 @@ const PillarCard : React.FC<PillarCardProps> = ({
             <div className="flex-grow text-lg font-medium text-gray-900 text-left">
                 {title}
             </div>
+
+            {/* Percentage Viewer */}
+            {generateViewPercentage(chapterID)!==100 && 
+            (<div className="flex items-center gap-2">
+                <div className="w-16 h-2 bg-gray-300 rounded-full overflow-hidden">
+                    <div
+                        className="h-full bg-green-500 transition-all duration-300"
+                        style={{ width: `${generateViewPercentage(chapterID)}%` }}
+                    ></div>
+                </div>
+                <div className="text-gray-700 font-semibold text-sm min-w-[40px]">
+                    {generateViewPercentage(chapterID)}%
+                </div>
+            </div>)}
 
             {/* Viewed Indicator */}
             <div onClick={handleToggleViewed} className="ml-4 cursor-pointer">
